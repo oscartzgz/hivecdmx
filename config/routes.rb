49ac9023bfa1243388ddc042703518903a15a73a
Oscar Tellez <oscartzgz@gmail.com>
2026-05-26 13:34:32 +0000

    feat: routes and admin base controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  resource :session
+  root "rooms#index"
+
+  resources :rooms,   only: [ :index, :show ]
+  resources :records, only: [ :update ] do
+    resources :photos, only: [ :create, :show ]
+  end
+  resources :reports, only: [ :show ] do
+    collection { get :export }
+  end
+
+  namespace :admin do
+    resources :users
+    resources :records, only: [ :index ]
+  end
+
+  # Generado por rails generate authentication:
+  resource  :session
   resources :passwords, param: :token
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
